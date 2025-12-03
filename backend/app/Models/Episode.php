8841e574bb584043bb7806c5ec3e6b4c1b3d0a7b
jackson-kishan <jackson.kishan.56@gmail.com>
2025-12-03 13:04:20 +0600

    Modify: Episode model

--- backend/app/Models/Episode.php
+++ backend/app/Models/Episode.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Episode extends Model
 {
@@ -20,4 +21,22 @@ class Episode extends Model
       "thumbnail_path"
     ];
   
+    protected $casts = [
+      "published_at" => "datetime",
+    ];
+
+    public function season(): BelongsTo
+    {
+        return $this->belongsTo(Season::class);
+    }
+
+    public function sources(): HasMany
+    {
+        return $this->hasMany(EpisodeSource::class);
+    }
+
+    public function PrimarySource(): HasOne
+    {
+        return $this->hasOne(EpisodeSource::class)->where("is_primary", true);
+    }
 }
