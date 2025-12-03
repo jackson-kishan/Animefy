89ed8721e8a8740a2060203c6ada4286cbe5bdb4
jackson-kishan <jackson.kishan.56@gmail.com>
2025-12-03 15:17:20 +0600

    Add: a api route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/api', function () {
+   return "Hello API";
+});
