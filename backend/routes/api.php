ca067e7e720203afd7216d8cc9c31cbff38fb054
jackson-kishan <jackson.kishan.56@gmail.com>
2025-12-03 15:22:15 +0600

    modify route file

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,10 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', fn () => response()->json(['message' => 'OK']));
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/api', function () {
-   return "Hello API";
-});
+Route::get('/api', fn() => response()->json(['message' => 'OK']));
