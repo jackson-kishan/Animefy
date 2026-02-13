b8ec0a8f0efc40db59c671e1e2a9b15f24c52171
Kishan Rozario <jackson.kishan.56@gmail.com>
2026-02-13 22:21:06 +0600

    create necessary route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\V1\AnimeController;
+use App\Http\Controllers\V1\EpisodeController;
+use App\Http\Controllers\V1\EpisodeSourceController;
+use App\Http\Controllers\V1\PlayBackController;
+use App\Http\Controllers\V1\SeasonController;
 
 Route::get('/', fn () => response()->json(['message' => 'OK']));
 
@@ -9,4 +14,15 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/api', fn() => response()->json(['message' => 'OK']));
+// Anime Resource
+Route::apiResource('animes', AnimeController::class);
+
+//
+Route::apiResource('animes.seasons', SeasonController::class);
+
+Route::apiResource('seasons.episodes', EpisodeController::class);
+
+Route::apiResource('episodes.sources', EpisodeSourceController::class);
+
+Route::get('episodes/{episode}/stream', [PlayBackController::class, 'stream']);
+
