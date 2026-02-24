5252e14f461c318e56d3ee9571b54f2fe07c294c
Kishan Rozario <jackson.kishan.56@gmail.com>
2026-02-24 21:24:25 +0600

    update api route & animeseeder

--- backend/database/seeders/AnimeSeeder.php
+++ backend/database/seeders/AnimeSeeder.php
@@ -3,9 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Anime;
-use App\Models\Episode;
-use App\Models\Season;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,33 @@
 <?php
 
+use App\Http\Controllers\V1\AnimeController;
+use App\Http\Controllers\V1\EpisodeController;
+use App\Http\Controllers\V1\EpisodeSourceController;
+use App\Http\Controllers\V1\PlayBackController;
+use App\Http\Controllers\V1\SeasonController;
+use App\Models\Anime;
+use App\Models\Season;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Anime Resource
+Route::apiResource('animes', AnimeController::class);
+
+// Anime Seasons Resource
+Route::apiResource('animes.seasons', SeasonController::class);
+Route::get('seasons', function (Anime $anime) {
+    $data = $anime->seasons()->get();
+    return response()->json($data);
+});
+
+// Season Episodes Resource
+Route::apiResource('animes.seasons.episodes', EpisodeController::class);
+
+// Episode Sources Resource
+Route::apiResource('animes.seasons.episodes.sources', EpisodeSourceController::class);
+
+// Route::get('episodes/{episode}/stream', [PlayBackController::class, 'stream']);
