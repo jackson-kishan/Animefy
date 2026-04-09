129f9e7b198319755ee40fae51a7f8e67581c46f
Kishan Rozario <jackson.kishan.56@gmail.com>
2026-04-09 20:47:44 +0600

    Modify anime model

--- backend/app/Models/Anime.php
+++ backend/app/Models/Anime.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Anime extends Model
@@ -29,5 +30,16 @@ class Anime extends Model
      {
         return $this->hasMany(Season::class);
         // ->orderBy('number')
+     }
+      public function episodes(): HasManyThrough
+     {
+        return $this->hasManyThrough(
+            Episode::class,
+            Season::class,
+            'anime_id',    // Foreign key on seasons table
+            'season_id',   // Foreign key on episodes table
+            'id',          // Local key on animes table
+            'id'           // Local key on seasons table
+        );
      }
 }
